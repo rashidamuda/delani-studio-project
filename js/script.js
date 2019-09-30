ab9b0e275dcad9b79fe06bd379eb46f7d08a0413
rashidamuda <rashid.amuda@gmail.com>
2019-09-30 10:04:01 +0300

    add third toggle function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 $(".show1").click(function() {
     $("#hide1").toggle();
-    $(".img-hidden").toggle();
+    $(".img-hidden1").toggle();
 });
 $(".show2").click(function() {
     $("#hide2").toggle();
-    $(".img-hidden").toggle();
+    $(".img-hidden2").toggle();
 });
-
+$(".show3").click(function() {
+    $("#hide3").toggle();
+    $(".img-hidden3").toggle();
+});
 });
