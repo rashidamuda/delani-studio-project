a770f02d3cfbc0f3275915b06a53a2ffa2978f4c
rashidamuda <rashid.amuda@gmail.com>
2019-09-30 11:21:05 +0300

    edit overlay function

--- js/script.js
+++ js/script.js
@@ -1,34 +1,58 @@
 $(document).ready(function() {
-$(".show1").click(function() {
+  $(".show1").click(function() {
     $("#hide1").toggle();
     $(".img-hidden1").toggle();
-});
-$(".show2").click(function() {
+  });
+
+  $(".show2").click(function() {
     $("#hide2").toggle();
     $(".img-hidden2").toggle();
-});
-$(".show3").click(function() {
+  });
+  $(".show3").click(function() {
     $("#hide3").toggle();
     $(".img-hidden3").toggle();
-});
-$(".img4").hover(function() {
+  });
+
+  $(".img4").hover(function() {
     $(".overlay4").show();
     $(".img4").mouseleave(function() {
-        $(".overlay4").hide();
+      $(".overlay4").hide();
     });
-$(".img1").hover(function() {
-        $(".overlay1").show();
-        $(".img1").mouseleave(function() {
-            $(".overlay1").hide();
-});
-$(".img2").hover(function() {
+  });
+  $(".img1").hover(function() {
+    $(".overlay1").show();
+    $(".img1").mouseleave(function() {
+      $(".overlay1").hide();
+    });
+  });
+  $(".img2").hover(function() {
     $(".overlay2").show();
     $(".img2").mouseleave(function() {
-        $(".overlay2").hide();
+      $(".overlay2").hide();
     });
-$(".img2").hover(function() {
-        $(".overlay2").show();
-        $(".img2").mouseleave(function() {
-            $(".overlay2").hide();
-        });
+  });
+  $(".img3").hover(function() {
+    $(".overlay3").show();
+    $(".img3").mouseleave(function() {
+      $(".overlay3").hide();
+    });
+  });
+  $(".img5").hover(function() {
+    $(".overlay5").show();
+    $(".img5").mouseleave(function() {
+      $(".overlay5").hide();
+    });
+  });
+  $(".img6").hover(function() {
+    $(".overlay6").show();
+    $(".img6").mouseleave(function() {
+      $(".overlay6").hide();
+    });
+  });
+  $(".img7").hover(function() {
+    $(".overlay7").show();
+    $(".img7").mouseleave(function() {
+      $(".overlay7").hide();
+    });
+  });
 });
